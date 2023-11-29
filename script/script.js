ad07ceec26111be56ea43a902b574ca0c624002d
tuananh <tuananhtran.xd@gmail.com>
2023-11-29 14:56:03 +0700

    add new animation

--- script/script.js
+++ script/script.js
@@ -6,9 +6,27 @@ function jump() {
     dino.classList.add('jump-animation');
     setTimeout(()=>{
         dino.classList.remove('jump-animation');
-    },250);
+    },500);
 
 }
-document.addEventListener('keypress',()=>{
-    jump();
+document.addEventListener('keypress',()=> {
+    if (!dino.classList.contains('jump-animation')){
+        jump();
+    }
 })
+
+setInterval(()=>{
+    score.innerText++;
+    const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+    const rockLeft = parseInt(window.getComputedStyle(rock).getPropertyValue('left'));
+    if (rockLeft<0){
+        rock.style.display = 'none';
+    } else {
+        rock.style.display ='';
+    }
+    if(rockLeft<175 && rockLeft>100 && dinoTop>175){
+        alert('Score: '+ (score.innerText - 1) + '\nPlay again?');
+        location.reload();
+    }
+
+},50)
